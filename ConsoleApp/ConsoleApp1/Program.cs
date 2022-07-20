e466daeeb822cb68d81580fda34aca2efda0dc68
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-20 15:51:41 -0300

    Multicast Delegate

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -4,6 +4,8 @@ using ConsoleApp1.Services;
 namespace ConsoleApp1
 {
     delegate double BinaryNumericOperation(double n1, double n2); // Criação do delegate.
+
+    delegate void BinaryVoid(double n1, double n2);
     class Program
     {
         static void Main(string[] args)
@@ -19,6 +21,13 @@ namespace ConsoleApp1
             double result = operation(a, b); // Passo os dois valores que o meu serviço pede para a classe rodar.
             //double result = operation.Invoke(a, b); // Sintaxe alternativa.
             Console.WriteLine(result); // Printar o resultado.
+
+            Console.WriteLine("\n-------------------------------\nBinary\n");
+
+            BinaryVoid bv = CalculationService.ShowSum;
+            bv += CalculationService.ShowMax; // Agora delegate está guardando referência para duas funções.
+
+            bv.Invoke(a, b); // Passa os valores a e b, depois printa as duas funções do delegate, na ordem, no caso primeiro ShowSum e depois o ShowMax
         }
     }
 }
