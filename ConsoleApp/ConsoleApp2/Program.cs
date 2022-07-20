41b4b0d4499d8a2bebae7f9ba9ae05eb4e865804
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-20 16:41:01 -0300

    Action

--- ConsoleApp/ConsoleApp2/Program.cs
+++ ConsoleApp/ConsoleApp2/Program.cs
@@ -1,9 +1,4 @@
-﻿// Problema exemplo
-// Fazer um programa que, a partir de uma lista de produtos, remova da
-// lista somente aqueles cujo preço mínimo seja 100.
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using ConsoleApp2.Entities;
 
@@ -13,6 +8,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            // Problema exemplo
+            // Fazer um programa que, a partir de uma lista de produtos, remova da
+            // lista somente aqueles cujo preço mínimo seja 100.
             List<Product> list = new List<Product>();
             list.Add(new Product("Tv", 900.00));
             list.Add(new Product("Mouse", 50.00));
@@ -25,10 +23,42 @@ namespace ConsoleApp2
             {
                 Console.WriteLine(p);
             }
+
+            Console.WriteLine("\n\nSegundo Exemplo:\n");
+            //Problema exemplo
+            //Fazer um programa que, a partir de uma lista de produtos, aumente o
+            //preço dos produtos em 10 %.
+            List<Product> listIncrease = new List<Product>();
+            listIncrease.Add(new Product("Tv", 900.00));
+            listIncrease.Add(new Product("Mouse", 50.00));
+            listIncrease.Add(new Product("Tablet", 350.50));
+            listIncrease.Add(new Product("HD Case", 80.90));
+
+            Console.WriteLine("Lista inicial:");
+            foreach (Product product in listIncrease)
+            {
+                Console.WriteLine(product);
+            }
+
+            Action<Product> act = UpdatePrice;
+            // Outra forma:
+            Action<Product> actIncrease = p => { p.Price += p.Price * 0.1; }; // Também pode ser colocado diretamente no listIncreave.ForEach.
+
+            Console.WriteLine("\nLista final:");
+            listIncrease.ForEach(act); // act pode ser também substituido por UpdatePrice.
+            foreach(Product product in listIncrease)
+            {
+                Console.WriteLine(product);
+            }
         }
         public static bool ProductTest(Product p)
         {
             return p.Price >= 100.00;
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
