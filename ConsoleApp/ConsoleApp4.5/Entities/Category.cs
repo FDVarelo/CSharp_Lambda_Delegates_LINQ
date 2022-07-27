58b291bdda7e8f2de61eb82a72b8a6233377a9ea
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-27 09:40:00 -0300

    Demo: LINQ com notação similar a SQL

---
+++ ConsoleApp/ConsoleApp4.5/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp4_5.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
