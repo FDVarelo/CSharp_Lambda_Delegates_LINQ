38c3a49bb4c59664f65eee2d4b70bbb2cf9f7a31
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-27 10:13:45 -0300

    Criação da Entidade Funcionario, e do arquivo .csv

---
+++ ConsoleApp/ExercicioFixacao/Entities/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioFixacao.Entities
+{
+    class Funcionario
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
